01788ac2972085a212a7f047f48497902f2d16ba
EkaterinaReva <ms.katty86@gmail.com>
2023-12-13 18:17:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
+import { photos } from './Carusel';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const [restaurants, setRestaurants] = useState(data);
+const [images, getImages] = useState(0);
+const{id,restName, image } = photos[images];
+
+
+const backImage =() => {
+  getImages((images => {
+    images --;
+    if (images < 0) {
+       return photos.length -1;
+      
+    }
+    return images;
+  }))
+}
+ const nextImage =() => {
+  getImages((images =>{
+    
+    images ++;
+    if(images > photos.length -1) {
+      images = 0
+    }
+    return images;
+  }))
+ }
+
+const removeRestaurant=(id) => {
+  let newRestaurant= restaurants.filter((restaurant) => restaurant.id !==id);
+  setRestaurants(newRestaurant);
+}
+
+return (
+    <div>
+      <div className="container"> 
+<h1>Подборка ресторанов для посещения в Москве.</h1>
+</div>
+{restaurants.map(( restaurant => {
+const {id, restName, description, image, place} = restaurant;
+  
+
+
+
+return(
+  <div key={id}>
+    <div className="container">
+<h2>{id}-{restName}</h2>
+</div>
+<div className="container">
+  <img src={image} width="300px" alt= "restaurant"/>
+</div>
+<div className='btn container'>
+  <button onClick={backImage}> Back</button>
+  <button onClick={nextImage}> Next</button>
+
+</div>
+<div className="container"> 
+<p> {description}</p>
+
+</div>
+<div className="container">
+  <p>{place}</p>
+</div>
+<div className="container">
+<button className='btn' onClick={() => removeRestaurant(id)}>Уже посетила</button>
+</div>
+
+  </div>
+)  
+}))}
+
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Carusel.js
@@ -0,0 +1,11 @@
+export const photos=[
+{
+    id: 1,
+    images:["https://thevanderlust.com/img/go/ry/gorynych_jpg_1530651348.jpg$i$min$822$530$cc$$.jpeg", "https://img03.rl0.ru/afisha/1894x1000i/s2.afisha.ru/mediastorage/5e/a8/3afb43e8f00e4b4d9931d10da85e.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStKH4J6_NhKnMKv_-vU6IFRYnt8MvI0iCJPg&usqp=CAU"]
+},
+{
+    id: 2,
+    images:["https://rekamoreokean.ru/wp-content/uploads/2016/03/3V7A0282-uai-516x344.jpg", "https://rekamoreokean.ru/wp-content/uploads/2016/03/3V7A0232-uai-516x344.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcT-cSzloylASbgrp7L8e1VafqEMZoQzS7FkltVwN9QD9ufuNDcm_0Hcply-u5xzeECIk&usqp=CAU"]
+}
+
+]
